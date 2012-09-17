7dc03ed21f86ce00d69e519bebe1387f946cf2d8
Adam Rosien <arosien@box.com>
2012-09-17 10:52:10 -0700

    Start of cmd-line option parser.

--- app/src/main/scala/App.scala
+++ app/src/main/scala/App.scala
@@ -12,21 +12,39 @@ class App extends xsbti.AppMain {
 object App {
   import net.rosien.sniff._
   
-  /** Shared by the launched version and the runnable version,
-   * returns the process status code */
   def run(args: Array[String]): Int = {
-    // TODO: cmd-line stuff
+    val opts = options(args.toList)
+    
     // TODO: String => Language
     val result = specs2.run(Scala.spec)
-    (0 /: result)( _ + _.hasIssues.fold(1, 0))
+    (0 /: result)(_ + _.hasIssues.fold(1, 0))
   }
   
-  /** Standard runnable class entrypoint */
   def main(args: Array[String]) {
     System.exit(run(args))
+  }
+  
+  type OptionMatch = PartialFunction[List[String], (List[String], (Symbol, String))]
+  val Param = """^--(\S+)=(.+)$""".r
+    
+  def arg(sym: Symbol, name: String): OptionMatch = {
+    case Param(n, value) :: tail if n == name => tail -> (sym -> value) 
+  }
+  
+  // stolen from StackOverflow, made better
+  def options(args: List[String], optionMatchers: OptionMatch*): Map[Symbol, String] = {
+    val default: PartialFunction[List[String], Map[Symbol, String]] = { 
+      case Nil => Map()
+      case Param(name, value) :: tail => sys.error("Unknown option '%s'".format(name))
+      case arg :: tail => sys.error("Unknown arg '%s'".format(arg))
+    }
+    
+    def wrap(optionMatch: OptionMatch): PartialFunction[List[String], Map[Symbol, String]] = optionMatch andThen {
+      case (tail, m) => options(tail, optionMatchers: _*) + m
+    }
+    
+    (optionMatchers.map(wrap _) :+ default).reduce(_ orElse _)(args)
   }
 }
 
 case class Exit(val code: Int) extends xsbti.Exit
-
-
