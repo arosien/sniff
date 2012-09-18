f832050ad4a92c42dd2e9aa28f3dd6c362d038cb
Adam Rosien <arosien@box.com>
2012-09-18 11:05:52 -0700

    Handle --version arg.

--- app/src/main/scala/App.scala
+++ app/src/main/scala/App.scala
@@ -13,26 +13,34 @@ object App {
   import net.rosien.sniff._
   
   def run(args: Array[String]): Int = {
-    val opts = options(args.toList)
-    
-    // TODO: String => Language
-    val result = specs2.run(Scala.spec)
-    (0 /: result)(_ + _.hasIssues.fold(1, 0))
+    val opts = options(args.toList,
+        arg('version, "version"))
+        
+    opts.get('version).map { version =>
+      println("sniff %s".format(BuildInfo.version))
+      0
+    }.getOrElse {
+      // TODO: String => Language
+      val result = specs2.run(Scala.spec)
+      (0 /: result)(_ + _.hasIssues.fold(1, 0))
+    }
   }
   
   def main(args: Array[String]) {
     System.exit(run(args))
   }
   
-  type OptionMatch = PartialFunction[List[String], (List[String], (Symbol, String))]
-  val Param = """^--(\S+)=(.+)$""".r
+  private type OptionMatch = PartialFunction[List[String], (List[String], (Symbol, String))]
+  private val Arg = """^--(\S+)$""".r
+  private val Param = """^--(\S+)=(.+)$""".r
     
-  def arg(sym: Symbol, name: String): OptionMatch = {
+  private def arg(sym: Symbol, name: String, default: String = ""): OptionMatch = {
+    case Arg(n) :: tail if n == name => tail -> (sym -> default)
     case Param(n, value) :: tail if n == name => tail -> (sym -> value) 
   }
   
   // stolen from StackOverflow, made better
-  def options(args: List[String], optionMatchers: OptionMatch*): Map[Symbol, String] = {
+  private def options(args: List[String], optionMatchers: OptionMatch*): Map[Symbol, String] = {
     val default: PartialFunction[List[String], Map[Symbol, String]] = { 
       case Nil => Map()
       case Param(name, value) :: tail => sys.error("Unknown option '%s'".format(name))
